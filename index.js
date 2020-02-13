5a4448c127f40a54b6724f3df281590feafad123
Matthew Martin <zhintateo19924559@gmail.com>
2020-02-12 21:58:48 -0600

    completed getModelYears

--- index.js
+++ index.js
@@ -195,8 +195,19 @@ function getCarInfoById(inventory, id) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * sortCarInventory returns an inventory that is sorted by car_model, ascending [A-Z].
 */
-function sortCarInventory(/* code here */) {
-  /* code here */
+function sortCarInventory(inventory) {
+  const sortedCarModels = inventory.sort(function (a, b) {
+    let nameA = a.car_model.toUpperCase();
+    let nameB = b.car_model.toUpperCase();
+    if (nameA < nameB){
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;  
+  });
+  return(sortedCarModels)
 }
 
 /**
@@ -208,8 +219,12 @@ function sortCarInventory(/* code here */) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(inventory) {
+  const getCarYear = [];
+  for (let i = 0; i < inventory.length; i++){
+    getCarYear.push(inventory[i].car_year)
+  };
+  return getCarYear;
 }
 
 /**
